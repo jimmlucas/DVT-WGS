c9c7411fb44ab3f98e894dd90d0c555dd01dd223
jimmlucas <jimmy_12222@hotmail.com>
2023-11-23 00:49:57 +0100

    update

--- bin/download_reads.sh
+++ bin/download_reads.sh
@@ -1,17 +1,18 @@
-#!/bin/bash                                                                            
+#!/bin/bash
 
-download_folder= "./data/in"
+download_folder="./data/in"
 
-if read -p "Please provide the full path to your Acc.list.txt file: " filepath; then
-    if [-s "$filepath" ]; then 
-    while IFS= read -r accesion; 
+if read -e -p "Please provide the full path to your Acc_List.txt file: " filepath && [ -s "$filepath" ]; then 
+    while IFS= read -r accession
     do
-        fastq-dump --split-files --gzip "$accession" -O "$dowload_folder" "$accesion"
+        echo "Processing accession: '$accession'"
+        if [ -n "$accession" ]; then
+            fastq-dump --split-files --gzip "$accession" -O "$download_folder"
+        else
+            echo "Skipping empty line."
+        fi
     done < "$filepath"
-    echo "Dowloaded files are stored in : $download_folder"
-    else
-    echo "ERROR: the specified file is empty or does not exist"
-    fi
+    echo "Downloaded files are stored in : $download_folder"
 else
-    echo "Error: No file path provided"
+    echo "ERROR: the specified file is empty or does not exist"
 fi
